497ee22f160b4bc7d513b727e663d21a9b0a1687
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-01-12 19:32:15 +0900

    ステップ2テスト実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
+
 RSpec.describe 'タスク管理機能', type: :system do
-  before do
-    #あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-    FactoryBot.create(:task)
-    #FactoryBot.create(:second_task)
-  end
+  let!(:task) {FactoryBot.create(:task) }
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -17,20 +15,30 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
   end
   describe '一覧表示機能' do
+    before do
+      #あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+      FactoryBot.create(:task)
+      FactoryBot.create(:second_task)
+      visit tasks_path
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, task_name: 'task')
+        expect(page).to have_content '名前1'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
         visit tasks_path
-        expect(page).to have_content 'task'
+        task_list = all('.task_row')
+        expect(task_list.first).to have_content '名前2'
       end
     end
   end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        @task = FactoryBot.create(:task, task_name: 'task2', task_content: 'テスト')
-        visit task_path(@task.id)
-        expect(page).to have_content 'テスト'
+        visit task_path(task.id)
+        expect(page).to have_content '名前1'
       end
     end
   end
