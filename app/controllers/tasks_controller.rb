289c2193ac4ca36c657d9ef0683a7d645ca6dbe3
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-01-18 17:17:34 +0900

    ユーザーモデル作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
     helper_method :sort_column, :sort_direction
 
     def index
-      @tasks = Task.order("#{sort_column} #{sort_direction}") .page(params[:pege]).per(5)
+      @tasks = Task.order("#{sort_column} #{sort_direction}") .page(params[:pege]).per(10)
       if params[:task_name].present?
         @tasks = @tasks.get_by_task_name params[:task_name]#.page(params[:pege]).per(5)
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_030919) do
+ActiveRecord::Schema.define(version: 2023_01_18_081038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2023_01_17_030919) do
     t.integer "status"
     t.integer "priority"
     t.index ["task_name", "status"], name: "index_tasks_on_task_name_and_status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
