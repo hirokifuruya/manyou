87e6d84e9a3348b5032c7c4a9662ba99a2e1f08a
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-01-25 23:47:21 +0900

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,21 @@ User.create(name: 'seed8', email: 'seed8@gmail.com', password: 'seed1@gmail.com'
 User.create(name: 'seed9', email: 'seed9@gmail.com', password: 'seed1@gmail.com', password_confirmation: 'seed1@gmail.com')
 User.create(name: 'seed10', email: 'seed10@gmail.com', password: 'seed1@gmail.com', password_confirmation: 'seed1@gmail.com')
 
-# <taskデータ>
-10.times do |i|
-  Task.create!(task_name: "タイトル", task_content: "テスト", deadline: '2023-01-25')
-end
 
 # <labelデータ>
 10.times do |i|
-  Label.create!(name: "LABEL")
+  Label.create!(name: "LABEL#{i + 1}")
+end
+
+# <taskデータ>
+10.times do |i|
+  Task.create!(
+    task_name: "タイトル#{1 + 1}",
+    task_content: "テスト",
+    deadline: '2023-01-25',
+    priority: rand(0..2),
+    status: rand(0..2),
+    user_id: 1,
+    label_ids: rand(1..10)
+  )
 end
