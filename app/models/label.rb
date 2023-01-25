cfa942091c0d920bdf5f3afcc058b5b858e22d65
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-01-25 17:18:15 +0900

    ラベル機能実装

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,4 @@
 class Label < ApplicationRecord
-  has_many :labelings, dependent: destroy
-  has_many :tasks, through: :labelings, source: :task
+  has_many :labelings, dependent: :destroy
+  has_many :tasks, through: :labelings
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :user
+
   validates :task_name, presence: true
   validates :task_content, presence: true
   validates :deadline, presence: true
+
+  has_many :labelings, dependent: :destroy
+  has_many :labels, through: :labelings
 
   enum status:{"未着手": 0, "着手中": 1, "完了": 2 }
   enum priority:{"低":0, "中":1, "高":2 }
